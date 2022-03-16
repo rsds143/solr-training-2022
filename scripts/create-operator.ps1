e280934421026ec0c8f530fd96d5414cb2181f8e
rsds143 <43972+rsds143@users.noreply.github.com>
2022-03-16 13:40:01 +0100

    Create create-operator.ps1

---
+++ scripts/create-operator.ps1
@@ -0,0 +1,2 @@
+Installs cert-manager kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
+Installs cass-operator which rights to every namespace kubectl apply -k github.com/k8ssandra/cass-operator/config/deployments/cluster?ref=v1.10.1
